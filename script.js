dcdf679582dd1b52dfb47531554f09f999f7c50b
livyavijesh10-ctrl <livyavijesh10@gmail.com>
2026-04-07 11:54:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAnimation() {
+  let text = document.getElementById("inputText").value;
+
+  document.getElementById("output").innerHTML =
+    "🎬 Generating animation for: <b>" + text + "</b>";
+}
